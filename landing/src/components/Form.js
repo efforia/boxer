a46da0b5a2298d8dc67fb82cd4fde826b5c44fe4
William Oliveira de Lagos <william.lagos@live.com>
2019-01-18 20:36:00 -0200

    Added new form handling

--- landing/src/components/Form.js
+++ landing/src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Form extends React.Component {
   subscribe() {
-    alert("Thanks!")
+    alert("Obrigado!")
   }
     render() {
         return (
@@ -30,10 +30,24 @@ class Form extends React.Component {
                 <h1><br />Eu quero um frete </h1>
                 <br />
                 <br />
-                <section className="subscribe">
-                  <input type="text" className="subscribe-input" id="subscribe-input" placeholder="Teu endereço de e-mail" />
+                <section className="subscribe" id="mc_embed_signup">
+                  <form action="https://fretefacil.us20.list-manage.com/subscribe/post?u=4acd3bfc9b0d30cdec21595e4&amp;id=09474778be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+                    <div id="mc_embed_signup_scroll">
+                      <div class="mc-field-group">
+                        {/*<label for="mce-EMAIL">Email Address </label>*/}
+                        <input type="email" value="" name="EMAIL" className="required email subscribe-input" id="mce-EMAIL" placeholder="Teu endereço de e-mail"/>
+                      </div>
+                      {/*<div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                      </div>
+                      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4acd3bfc9b0d30cdec21595e4_09474778be" tabindex="-1" value=""></div>*/}
+                      <div class="clear">
+                        <input type="submit" value="Saber mais" name="subscribe" id="mc-embedded-subscribe" className="btn-subscribe"/>
+                      </div>
+                    </div>
+                  </form>
                 </section>
-                <button className="btn-subscribe" id="subscribe-btn"> Saber mais  </button>
               </div>
               <br />
               <br />
